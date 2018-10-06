69ad1d6b4ee839516b9d6b9fa01a07e59b772732
LaurentBerger <laurent.berger@univ-lemans.fr>
2018-10-06 16:54:04 +0200

    typo

--- buildwxHighguisample.sh
+++ buildwxHighguisample.sh
@@ -1,21 +1,19 @@
 #!/bin/bash -e
-myRepo=$(pwd)
-
-CMAKE_CONFIG_GENERATOR="Visual Studio 15 2017 Win64"
+source ./libmodel.sh
 RepoSource=sample
-cd Build/$RepoSource
+pushd ${buildRepo}/$RepoSource
 cmake -G"$CMAKE_CONFIG_GENERATOR"   \
--DBUILD_SHARED_LIBS:BOOL=OFF \
--DwxWidgets_ROOT_DIR=${myRepo}/install/wxwidgets -DwxWidgets_LIB_DIR=${myRepo}/install/wxwidgets/lib/vc_x64_lib \
--Dopencv_DIR=${myRepo}/install/opencv/x64/vc15/staticlib \
--DwxNanoLib_DIR=${myRepo}/install/wxnano/lib/cmake/wxNanoLib \
-../../"$RepoSource"
+-DBUILD_SHARED_LIBS:BOOL=${SharedLibs} \
+-DwxWidgets_ROOT_DIR=${myRepo}/install/wxwidgets -DwxWidgets_LIB_DIR=${installRepo}/wxwidgets/lib/${wxlibs} \
+-Dopencv_DIR=${installRepo}/opencv/${ocvlibs} \
+-DwxNanoLib_DIR=${myRepo}/install/wxnano/lib/cmake/wxHighguiLib \
+${sourceRepo}/"$RepoSource"
 
 
 cmake --build .  --config release 
 echo "************************* $Source_DIR -->devenv debug"
 cmake --build .  --config debug 
-cd ..
+popd
 
 
 
